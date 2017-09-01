50899949fe85985fef14ee2784d8e82b0537a59e
Edna Carvalho <ednacarvalhosempre@gmail.com>
2017-09-01 10:56:00 -0300

    Adição do Util

---
+++ SistemaDelivery/SistemaDelivery/Util/SessionHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+
+namespace SistemaDelivery.Util
+{
+    public enum SessionKeys { Empresa, Usuario}
+
+    public static class SessionHelper
+    {
+        public static Object Get(SessionKeys chave)
+        {
+            String chaveString = Enum.GetName(typeof(SessionKeys),chave);
+            return HttpContext.Current.Session[chaveString];
+        }
+
+        public static Object Set(SessionKeys chave,Object valor)
+        {
+            String chaveString = Enum.GetName(typeof(SessionKeys), chave);
+            return HttpContext.Current.Session[chaveString] = valor;
+        }
+    }
+}
